642cba58f6f9a1fd8df149657ebf1fcb78baffc9
Bence Kollanyi <kollanyi@gmail.com>
2016-04-05 22:26:49 +0200

    Minor formatting

--- plot.py
+++ plot.py
@@ -18,50 +18,41 @@ testData = pd.DataFrame( {'x':x, 'y':y} )
 # it looks just like a dataframe from R
 print(testData)
 
-# Next, you make an robject containing function that makes the plot.
-# the language in the function is pure R, so it can be anything
-# note that the R environment is blank to start, so ggplot2 has to be
-# loaded
+## Make an robject containing function that makes the plot.
 plotFunc = robj.r("""
- library(ggplot2)
+library(ggplot2)
 
 function(df){
- p <- ggplot(df, aes(x, y)) +
- geom_point( )
-
-print(p)
- }
+      p <- ggplot(df, aes(x, y)) + geom_point()
+      print(p)
+      }
 """)
 
-# import graphics devices. This is necessary to shut the graph off
-# otherwise it just hangs and freezes python
+## Import graphics devices.
 gr = importr('grDevices')
 
-# convert the testData to an R dataframe
+## Convert the testData to an R dataframe
 robj.pandas2ri.activate()
 testData_R = robj.conversion.py2ri(testData)
 
-# run the plot function on the dataframe
+## Run the plot function on the dataframe
 plotFunc(testData_R)
 
-# ask for input. This requires you to press enter, otherwise the plot
-# window closes immediately
+## This requires you to press enter, otherwise the plot window closes
 raw_input()
 
-# shut down the window using dev_off()
+## Shut down the window using dev_off()
 gr.dev_off()
 
-# you can even save the output once you like it
+## Save the output in a pdf
 plotFunc_2 = robj.r("""
 library(ggplot2)
+
 function(df){
- p <- ggplot(df, aes(x, y)) +
- geom_point( ) +
- theme(
- panel.background = element_rect(fill = NA, color = 'black')
- )
- ggsave('rpy2_magic.pdf', plot = p, width = 6.5, height = 5.5)
- }
+      p <- ggplot(df, aes(x, y)) + geom_point( ) +
+            theme(panel.background = element_rect(fill = NA, color = 'black'))
+      ggsave('rpy2_magic.pdf', plot = p, width = 6.5, height = 5.5)
+      }
 """)
 
 plotFunc_2(testData_R)
